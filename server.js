7de4ee7f405c7b2f8a52a6fbdb82b5dfdb7c60ac
Fábio Verardino <fabio@verardino.dev>
2023-04-26 15:11:51 -0300

    Setup serving files in public folder

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
     res.send("Hello, world!");
-});
+});*/
+
+// Serve files in public folder
+app.use(express.static('public'));
 
 const PORT = process.env['PORT'] || 3000;
 app.listen(PORT);
