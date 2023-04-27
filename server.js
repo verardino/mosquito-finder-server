08816dee79d4a42e761ae643eb46907016f59a96
Fábio Verardino <fabio@verardino.dev>
2023-04-27 10:14:16 -0300

    Added API route handler

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-/*app.get('/', (req, res) => {
-    res.send("Hello, world!");
-});*/
+// Handle api routes
+const apiHandler = require('./api/api');
+app.use('/api', apiHandler);
 
 // Serve files in public folder
 app.use(express.static('public'));
 
 const PORT = process.env['PORT'] || 3000;
 app.listen(PORT);
+console.log(`Server listening on port ${PORT}...`);
